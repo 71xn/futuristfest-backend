b9c40af72865640e0749af461b9196ce4d66859b
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2021-10-05 20:40:03 +0100

    got the api to work with cors

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-# app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/")
+@cross_origin()
+def helloWorld():
+    return "Hello, cross-origin-world!"
+
 
 @app.route('/getmsg/', methods=['GET'])
+@cross_origin()
 def respond():
     # Retrieve the name from url parameter
     name = request.args.get("name", None)
@@ -10,42 +21,7 @@ def respond():
     # For debugging
     print(f"got name {name}")
 
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    response = {"message":f"Hello {name}"}
 
     # Return the response in json format
     return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
